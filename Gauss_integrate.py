eaa7a68542f1d57cbb5e6fac4015fd94e27e0c3e
leo kavanagh <leo.kavanagh@yahoo.co.uk>
2025-08-16 18:07:57 +0100

    finished S matrix

--- Gauss_integrate.py
+++ Gauss_integrate.py
@@ -8,8 +8,15 @@ from scipy.integrate import simps
 e = 1.602*10**(-19)
 hbar = 6.626*10 ** (-34) * 1 / (2 * np.pi)
 
-def Smat_gauss(r, alpha, N):
+def Smat_gauss(R, alpha, N):
+    out_matrix = np.zeros([N, N])
     for n in range(N):
         for m in range(N):
+            prefactor = pi / (alpha[n] + alpha[m]) ** (3/2)
+            exponential = -alpha[n] * alpha[m] / (alpha[n] + alpha[m]) * (R[n] - R[m]) ** 2
+            out_matrix[n, m] = prefactor * np.exp(exponential)
+    return out_matrix
+
+
             
     
