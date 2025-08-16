605572e512685c08cde35a6d7021489d74da7cb9
leo kavanagh <leo.kavanagh@yahoo.co.uk>
2025-08-16 17:53:57 +0100

    Initial commit

---
+++ Gauss_integrate.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from numpy import inf, nan
+import numpy.pi as pi
+from scipy.integrate import simps
+
+e = 1.602*10**(-19)
+hbar = 6.626*10 ** (-34) * 1 / (2 * np.pi)
+
+def Smat_gauss(r, alpha, N):
+    for n in range(N):
+        for m in range(N):
+            
+    
