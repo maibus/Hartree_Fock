023730e16250ade85e5aab40d284e7a1ae2de06a
leo kavanagh <leo.kavanagh@yahoo.co.uk>
2025-08-17 11:48:45 +0100

    initial matrix computation

--- Gauss_integrate.py
+++ Gauss_integrate.py
@@ -10,24 +10,24 @@ e = 1.602*10**(-19)
 hbar = 6.626*10 ** (-34) * 1 / (2 * np.pi)
 
 
-def Smat_gauss(R, alpha, N):
+def Smat_gauss(R, alpha, C, N):
     out_matrix = np.zeros([N, N])
     for n in range(N):
         for m in range(N):
             prefactor = pi / (alpha[n] + alpha[m]) ** (3/2)
             exponential = -alpha[n] * alpha[m] / (alpha[n] + alpha[m]) * (R[n] - R[m]) ** 2
-            out_matrix[n, m] = prefactor * np.exp(exponential)
+            out_matrix[n, m] = prefactor * np.exp(exponential) * C[n] * C[m]
     return out_matrix
 
 
-def Tmat_gauss(R, alpha, N, Smat):
+def Tmat_gauss(R, alpha, C, N, Smat):
     out_matrix = np.zeros([N, N])
     for n in range(N):
         for m in range(N):
             X = alpha[n] * alpha[m] / (alpha[n] + alpha[m])  # just a repeated part
             prefactor = 0.5 * X * Smat[n, m]
             main = (6 - 4 * X * (R[m] - R[n]) ** 3)
-            out_matrix[n, m] = prefactor * main
+            out_matrix[n, m] = prefactor * main * C[n] * C[m]
     return out_matrix
             
 
@@ -41,7 +41,7 @@ def Amat_gauss(R, Rmat, alpha, N, Smat):
     return out_matrix
 
 
-def Qmat_gauss(Rmat, alpha, N, Smat):
+def Qmat_gauss(Rmat, alpha, C, N, Smat):
     out_matrix = np.zeros([N, N, N, N])
     for m in range(N):
         for n in range(N):
@@ -51,7 +51,7 @@ def Qmat_gauss(Rmat, alpha, N, Smat):
                     p1 = alpha[m] + alpha[o]
                     p2 = alpha[n] + alpha[p]
                     main = p1 * p2 / (p1 + p2) * np.abs(R[m, o] - R[m, p])
-                    out_matrix[m, n, o, p] = prefactor * erf(main)
+                    out_matrix[m, n, o, p] = prefactor * erf(main) * C[m] * C[n] * C[o] * C[p]
     return out_matrix
 
 
--- GuassHF.py
+++ GuassHF.py
@@ -5,6 +5,7 @@ from numpy import inf, nan
 import numpy.pi as pi
 from scipy.integrate import simps
 from math import erf
+from Gauss_integrate import Smat_gauss, Tmat_gauss, Amat_gauss, Qmat_gauss, Rmat_gauss
 
 e = 1.602*10**(-19)
 hbar = 6.626*10 ** (-34) * 1 / (2 * np.pi)
@@ -12,4 +13,22 @@ m_e = 9.109 * 10 ** (-31)
 m_p = 1.673 * 10 ** (-27)
 epsilon_0 = 8.854 * 10 ** (-12)
 a_0 = (4 * np.pi * epsilon_0 * hbar ** 2) / (m_e * e ** 2)
+
+
+R = np.array([0, 0.8 * 10 ** (-10)])  # initialise positions of ions
+Z = np.array([1, 2])  # initialise ion charges
+N = 2  # number of electrons
+
+
+basis = np.zeros([2, 3])  # 3 entries per wavefunction - C, alpha, R
+basis[0, :] = np.array([0.3696, 0.4166, R[0]])
+basis[1, :] = np.array([0.5881, 0.7739, R[1]])
+
+
+Rmatrix = Rmat_gauss(basis[:, 2], basis[:, 1])
+S = Smat_gauss(basis[:, 2], basis[:, 1], basis[:, 0], N)
+T = Tmat_gauss(basis[:, 2], basis[:, 1], basis[:, 0], N, S)
+A = Amat_gauss(R, Rmatrix, basis[:, 1], N, S)
+Q = Qmat_gauss(Rmatrix, basis[:, 1], basis[:, 0], N, S)
+
 
