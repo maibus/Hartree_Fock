f3684ba63f72ffa58c12e3bc5621b4859e8eb2ed
leo kavanagh <leo.kavanagh@yahoo.co.uk>
2025-09-04 00:30:45 +0100

    made radial integrator output matrices

--- Rad_integrate.py
+++ Rad_integrate.py
@@ -14,30 +14,62 @@ def laplacian(r, f, dr):
     return output
 
 
-def getS(f1, f2, r, dr):
-    integrand = f1 * f2
-    J = 4 * np.pi * r ** 2
-    return simps(J * integrand, r)
+def getS(basis, r, dr):
+    N = np.shape(basis)[0]
+    output = np.zeros([N, N])
+    for m in range(N):
+        for n in range(N):
+            f1 = basis[m, :]
+            f2 = basis[n, :]
+            integrand = f1 * f2
+            J = 4 * np.pi * r ** 2
+            output[m, n] = simps(J * integrand, r)
+    return output
 
 
-def getT(f1, f2, r, dr):
-    integrand = f1 * laplacian(r, f2, dr)
-    J = 4 * np.pi * r ** 2
-    return simps(J * integrand, r)
+def getT(basis, r, dr):
+    N = np.shape(basis)[0]
+    output = np.zeros([N, N])
+    for m in range(N):
+        for n in range(N):
+            f1 = basis[m, :]
+            f2 = basis[n, :]            
+            integrand = f1 * laplacian(r, f2, dr)
+            J = 4 * np.pi * r ** 2
+            output[m, n] = simps(J * integrand, r)
+    return output
 
 
-def getA(R, Z, f1, f2, r, dr):
-    R_index = int(R / dr)
-    rho = f1 * f2
-    inner = (1 / r) * np.sum((rho * r ** 2)[:R_index] * dr)
-    outer = np.sum((rho * r)[R_index:] * dr)
-    integral = 4 * np.pi * (inner + outer)
-    return integral
+def getA(R, Z, basis, r, dr):
+    N = np.shape(basis)[0]
+    output = np.zeros([N, N])
+    for a in range(np.size(Z)):
+        for m in range(N):
+            for n in range(N):
+                f1 = basis[m, :]
+                f2 = basis[n, :]
+                R_index = int(R[a] / dr)
+                rho = f1 * f2
+                inner = (1 / r) * np.sum((rho * r ** 2)[:R_index] * dr)
+                outer = np.sum((rho * r)[R_index:] * dr)
+                integral = 4 * np.pi * (inner + outer)
+                output[m, n] += Z * integral
+    return output
 
-def getQ(f1, f2, f3, f4, r, dr):
-    rho1 = f1 * f2
-    rho2 = f3 * f4
-    inner = (1 / r) * np.cumsum(rho1 * r ** 2) * dr
-    outer = np.sum(rho1 * r) * dr - np.cumsum(rho1 * r) * dr
-    integrand = 4 * np.pi * r ** 2 * rho2 * 4 * np.pi * (inner + outer)
-    return simps(integrand, r)
+
+def getQ(basis, r, dr):
+    N = np.shape(basis)[0]
+    output = np.zeros([N, N])
+    for m in range(N):
+        for n in range(N):
+            for o in range(N):
+                for p in range(N):
+                    rho1 = f1 * f2
+                    rho2 = f3 * f4
+                    inner = (1 / r) * np.cumsum(rho1 * r ** 2) * dr
+                    outer = np.sum(rho1 * r) * dr - np.cumsum(rho1 * r) * dr
+                    integrand = 4 * np.pi * r ** 2 * rho2 * 4 * np.pi * (inner + outer)
+                    output[m, n, o, p] = simps(integrand, r)
+    return output
+
+
